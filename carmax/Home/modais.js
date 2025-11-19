d741a3a688d734e77b1aeebc2a06dd544444cecf
mathtz <mmatheusmorais897@gmail.com>
2025-11-19 20:50:19 -0300

    Add files via upload

---
+++ carmax/Home/modais.js
@@ -0,0 +1,358 @@
+// modais
+
+window.onclick = (event) => {
+  if (event.target.classList.contains("modal")) {
+    event.target.style.display = "none";
+  }
+};
+
+document.querySelectorAll(".close").forEach((btn) => {
+  btn.onclick = () => {
+    const modalId = btn.getAttribute("data-modal");
+    document.getElementById(modalId).style.display = "none";
+  };
+});
+
+// Inicio Modais Romance
+document.getElementById("bntR01").onclick = () => {
+  document.getElementById("modalR01").style.display = "block";
+};
+
+document.getElementById("bntR02").onclick = () => {
+  document.getElementById("modalR02").style.display = "block";
+};
+
+document.getElementById("bntR03").onclick = () => {
+  document.getElementById("modalR03").style.display = "block";
+};
+
+document.getElementById("bntR04").onclick = () => {
+  document.getElementById("modalR04").style.display = "block";
+};
+
+document.getElementById("bntR05").onclick = () => {
+  document.getElementById("modalR05").style.display = "block";
+};
+
+document.getElementById("bntR06").onclick = () => {
+  document.getElementById("modalR06").style.display = "block";
+};
+
+document.getElementById("bntR07").onclick = () => {
+  document.getElementById("modalR07").style.display = "block";
+};
+
+document.getElementById("bntR08").onclick = () => {
+  document.getElementById("modalR08").style.display = "block";
+};
+
+document.getElementById("bntR09").onclick = () => {
+  document.getElementById("modalR09").style.display = "block";
+};
+
+document.getElementById("bntR10").onclick = () => {
+  document.getElementById("modalR10").style.display = "block";
+};
+
+// Fim Modais Romance
+
+// Inicio Modais Terror
+document.getElementById("bntT01").onclick = () => {
+  document.getElementById("modalT01").style.display = "block";
+};
+
+document.getElementById("bntT02").onclick = () => {
+  document.getElementById("modalT02").style.display = "block";
+};
+
+document.getElementById("bntT03").onclick = () => {
+  document.getElementById("modalT03").style.display = "block";
+};
+
+document.getElementById("bntT04").onclick = () => {
+  document.getElementById("modalT04").style.display = "block";
+};
+
+document.getElementById("bntT05").onclick = () => {
+  document.getElementById("modalT05").style.display = "block";
+};
+
+document.getElementById("bntT06").onclick = () => {
+  document.getElementById("modalT06").style.display = "block";
+};
+
+document.getElementById("bntT07").onclick = () => {
+  document.getElementById("modalT07").style.display = "block";
+};
+
+document.getElementById("bntT08").onclick = () => {
+  document.getElementById("modalT08").style.display = "block";
+};
+
+document.getElementById("bntT09").onclick = () => {
+  document.getElementById("modalT09").style.display = "block";
+};
+
+document.getElementById("bntT10").onclick = () => {
+  document.getElementById("modalT10").style.display = "block";
+};
+
+// Fim Modais Terror
+
+// Inicio Modais Comedia
+document.getElementById("bntC01").onclick = () => {
+  document.getElementById("modalC01").style.display = "block";
+};
+
+document.getElementById("bntC02").onclick = () => {
+  document.getElementById("modalC02").style.display = "block";
+};
+
+document.getElementById("bntC03").onclick = () => {
+  document.getElementById("modalC03").style.display = "block";
+};
+
+document.getElementById("bntC04").onclick = () => {
+  document.getElementById("modalC04").style.display = "block";
+};
+
+document.getElementById("bntC05").onclick = () => {
+  document.getElementById("modalC05").style.display = "block";
+};
+
+document.getElementById("bntC06").onclick = () => {
+  document.getElementById("modalC06").style.display = "block";
+};
+
+document.getElementById("bntC07").onclick = () => {
+  document.getElementById("modalC07").style.display = "block";
+};
+
+document.getElementById("bntC08").onclick = () => {
+  document.getElementById("modalC08").style.display = "block";
+};
+
+document.getElementById("bntC09").onclick = () => {
+  document.getElementById("modalC09").style.display = "block";
+};
+
+document.getElementById("bntC10").onclick = () => {
+  document.getElementById("modalC10").style.display = "block";
+};
+
+// Fim Modais Comedia
+
+// Inicio Modais Drama
+document.getElementById("bntDR01").onclick = () => {
+  document.getElementById("modalDR01").style.display = "block";
+};
+
+document.getElementById("bntDR02").onclick = () => {
+  document.getElementById("modalDR02").style.display = "block";
+};
+
+document.getElementById("bntDR03").onclick = () => {
+  document.getElementById("modalDR03").style.display = "block";
+};
+
+document.getElementById("bntDR04").onclick = () => {
+  document.getElementById("modalDR04").style.display = "block";
+};
+
+document.getElementById("bntDR05").onclick = () => {
+  document.getElementById("modalDR05").style.display = "block";
+};
+
+document.getElementById("bntDR06").onclick = () => {
+  document.getElementById("modalDR06").style.display = "block";
+};
+
+document.getElementById("bntDR07").onclick = () => {
+  document.getElementById("modalDR07").style.display = "block";
+};
+
+document.getElementById("bntDR08").onclick = () => {
+  document.getElementById("modalDR08").style.display = "block";
+};
+
+document.getElementById("bntDR09").onclick = () => {
+  document.getElementById("modalDR09").style.display = "block";
+};
+
+document.getElementById("bntDR10").onclick = () => {
+  document.getElementById("modalDR10").style.display = "block";
+};
+
+// Fim Modais Drama
+
+// Inicio Modais Documentario
+document.getElementById("bntD01").onclick = () => {
+  document.getElementById("modalD01").style.display = "block";
+};
+
+document.getElementById("bntD02").onclick = () => {
+  document.getElementById("modalD02").style.display = "block";
+};
+
+document.getElementById("bntD03").onclick = () => {
+  document.getElementById("modalD03").style.display = "block";
+};
+
+document.getElementById("bntD04").onclick = () => {
+  document.getElementById("modalD04").style.display = "block";
+};
+
+document.getElementById("bntD05").onclick = () => {
+  document.getElementById("modalD05").style.display = "block";
+};
+
+document.getElementById("bntD06").onclick = () => {
+  document.getElementById("modalD06").style.display = "block";
+};
+
+document.getElementById("bntD07").onclick = () => {
+  document.getElementById("modalD07").style.display = "block";
+};
+
+document.getElementById("bntD08").onclick = () => {
+  document.getElementById("modalD08").style.display = "block";
+};
+
+document.getElementById("bntD09").onclick = () => {
+  document.getElementById("modalD09").style.display = "block";
+};
+
+document.getElementById("bntD10").onclick = () => {
+  document.getElementById("modalD10").style.display = "block";
+};
+
+// Fim Modais Documentario
+
+// Inicio Modais Suspense
+document.getElementById("bntS01").onclick = () => {
+  document.getElementById("modalS01").style.display = "block";
+};
+
+document.getElementById("bntS02").onclick = () => {
+  document.getElementById("modalS02").style.display = "block";
+};
+
+document.getElementById("bntS03").onclick = () => {
+  document.getElementById("modalS03").style.display = "block";
+};
+
+document.getElementById("bntS04").onclick = () => {
+  document.getElementById("modalS04").style.display = "block";
+};
+
+document.getElementById("bntS05").onclick = () => {
+  document.getElementById("modalS05").style.display = "block";
+};
+
+document.getElementById("bntS06").onclick = () => {
+  document.getElementById("modalS06").style.display = "block";
+};
+
+document.getElementById("bntS07").onclick = () => {
+  document.getElementById("modalS07").style.display = "block";
+};
+
+document.getElementById("bntS08").onclick = () => {
+  document.getElementById("modalS08").style.display = "block";
+};
+
+document.getElementById("bntS09").onclick = () => {
+  document.getElementById("modalS09").style.display = "block";
+};
+
+document.getElementById("bntS10").onclick = () => {
+  document.getElementById("modalS10").style.display = "block";
+};
+
+// Fim Modais Suspense
+
+// Inicio Modais Ação
+document.getElementById("bntA01").onclick = () => {
+  document.getElementById("modalA01").style.display = "block";
+};
+
+document.getElementById("bntA02").onclick = () => {
+  document.getElementById("modalA02").style.display = "block";
+};
+
+document.getElementById("bntA03").onclick = () => {
+  document.getElementById("modalA03").style.display = "block";
+};
+
+document.getElementById("bntA04").onclick = () => {
+  document.getElementById("modalA04").style.display = "block";
+};
+
+document.getElementById("bntA05").onclick = () => {
+  document.getElementById("modalA05").style.display = "block";
+};
+
+document.getElementById("bntA06").onclick = () => {
+  document.getElementById("modalA06").style.display = "block";
+};
+
+document.getElementById("bntA07").onclick = () => {
+  document.getElementById("modalA07").style.display = "block";
+};
+
+document.getElementById("bntA08").onclick = () => {
+  document.getElementById("modalA08").style.display = "block";
+};
+
+document.getElementById("bntA09").onclick = () => {
+  document.getElementById("modalA09").style.display = "block";
+};
+
+document.getElementById("bntA10").onclick = () => {
+  document.getElementById("modalA10").style.display = "block";
+};
+
+// Fim Modais Ação
+
+// Inicio Modais Ação
+document.getElementById("bntF01").onclick = () => {
+  document.getElementById("modalF01").style.display = "block";
+};
+
+document.getElementById("bntF02").onclick = () => {
+  document.getElementById("modalF02").style.display = "block";
+};
+
+document.getElementById("bntF03").onclick = () => {
+  document.getElementById("modalF03").style.display = "block";
+};
+
+document.getElementById("bntF04").onclick = () => {
+  document.getElementById("modalF04").style.display = "block";
+};
+
+document.getElementById("bntF05").onclick = () => {
+  document.getElementById("modalF05").style.display = "block";
+};
+
+document.getElementById("bntF06").onclick = () => {
+  document.getElementById("modalF06").style.display = "block";
+};
+
+document.getElementById("bntF07").onclick = () => {
+  document.getElementById("modalF07").style.display = "block";
+};
+
+document.getElementById("bntF08").onclick = () => {
+  document.getElementById("modalF08").style.display = "block";
+};
+
+document.getElementById("bntF09").onclick = () => {
+  document.getElementById("modalF09").style.display = "block";
+};
+
+document.getElementById("bntF10").onclick = () => {
+  document.getElementById("modalF10").style.display = "block";
+};
+
+// Fim Modais Ação
